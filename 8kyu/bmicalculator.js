eee501636600c621bc8f6bc4511d19a4a90aa67a
Tim Leung <timleungtech@gmail.com>
2022-05-02 09:30:31 -0400

    add solution

---
+++ 8kyu/bmicalculator.js
@@ -0,0 +1,13 @@
+//Calculate BMI
+function bmi(weight, height) {
+    let bmi = weight / (height * height)
+    if (bmi <= 18.5){
+      return "Underweight"
+    } else if (bmi <= 25){
+      return "Normal"
+    } else if (bmi <= 30){
+      return "Overweight"
+    } else {
+      return "Obese"
+    }
+  }
