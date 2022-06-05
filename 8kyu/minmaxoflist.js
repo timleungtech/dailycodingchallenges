08d020ccfb0580a06b2a2cc54ccd2bd0cd4cbdbb
Tim Leung <timleungtech@gmail.com>
2022-06-05 07:51:16 -0400

    add solution

---
+++ 8kyu/minmaxoflist.js
@@ -0,0 +1,9 @@
+//Find Maximum and Minimum Values of a List
+var min = function(list){
+    list.sort((a,b) => a - b)
+    return list[0];
+}
+var max = function(list){  
+    list.sort((a,b) => a - b)
+    return list[list.length - 1];
+}
