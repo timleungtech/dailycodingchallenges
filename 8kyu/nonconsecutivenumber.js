6853a820d6685974746e9d68c340f6ebf2c925f4
Tim Leung <timleungtech@gmail.com>
2022-06-27 01:25:23 -0400

    add solution

---
+++ 8kyu/nonconsecutivenumber.js
@@ -0,0 +1,9 @@
+//Find the first non-consecutive number
+function firstNonConsecutive (arr) {
+    for (let i = 1; i < arr.length; i++) {
+      if (arr[i] !== arr[i-1] + 1){
+        return arr[i]
+      }
+    }
+    return null
+}
