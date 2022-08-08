906782af5b5f8c0ec24fff92e89b2ad61d6f72f1
Tim Leung <timleungtech@gmail.com>
2022-08-08 12:08:19 -0400

    add solution

---
+++ 8kyu/arrayfilter2.js
@@ -0,0 +1,5 @@
+//Filter out the geese
+function gooseFilter (birds) {
+    var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+    return birds.filter(x => !geese.includes(x))
+}
