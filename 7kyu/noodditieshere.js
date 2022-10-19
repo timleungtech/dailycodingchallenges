f7149117bd327ecd11d9b1771cd6b22ebbfab1be
Tim Leung <timleungtech@gmail.com>
2022-10-19 00:28:04 -0400

    add solution

---
+++ 7kyu/noodditieshere.js
@@ -0,0 +1,10 @@
+//No oddities here
+function noOdds(values){
+    let arr = []
+    for (let i = 0; i < values.length; i++){
+      if (values[i] % 2 === 0){
+        arr.push(values[i])
+      }
+    }
+    return arr
+}
