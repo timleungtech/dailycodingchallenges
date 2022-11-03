1948ef7f7758b997b9132bbabaa1c7d242ffc235
Tim Leung <timleungtech@gmail.com>
2022-11-03 00:30:55 -0400

    add solution

---
+++ 7kyu/countdivisorsofnum.js
@@ -0,0 +1,10 @@
+//Count the divisors of a number
+function getDivisorsCnt(n){
+    let count = 0
+    for (let i = 1; i <= n; i++){
+      if (n % i == 0){
+        count = count + 1
+      }
+    }
+    return count
+}
