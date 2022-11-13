a0d72570327468a62afa4c35eeda8826ea787f86
Tim Leung <timleungtech@gmail.com>
2022-11-13 07:28:00 -0500

    add solution

---
+++ 6kyu/isanumberprime.js
@@ -0,0 +1,10 @@
+//Is a number prime?
+function isPrime(num) {
+    if (num < 2) return false
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+      if (num % i == 0) {
+        return false
+      }
+    }
+    return true
+}
