ef2d1f6a8ad888b58fdddb86500d155e68c0f6f9
Tim Leung <timleungtech@gmail.com>
2022-11-16 00:05:24 -0500

    add solution

---
+++ 7kyu/findthedivisors.js
@@ -0,0 +1,11 @@
+//Find the divisors!
+function divisors(integer) {
+    let arr = []
+    for (let i = 2; i < integer; i++){
+      if (integer % i == 0) {
+        arr.push(i)
+      }
+    }
+    if (arr.length == 0) return `${integer} is prime`
+    else return arr
+}
