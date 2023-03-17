89f60e8f76eeefc4927f76d7f7842b3893f1e8ed
timleungtech <hellopwntchu@gmail.com>
2023-03-17 00:10:43 -0400

    add solution

---
+++ 7kyu/predictyourage.js
@@ -0,0 +1,4 @@
+//Predict your age!
+function predictAge(age1,age2,age3,age4,age5,age6,age7,age8){
+  return Math.floor(((age1**2 + age2**2 + age3**2 + age4**2 + age5**2 + age6**2 + age7**2 + age8**2)**.5)/2)
+}
