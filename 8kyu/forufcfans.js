791ce3d6b4dd5db6491b22ddad9680b3e351dd5f
timleungtech <hellopwntchu@gmail.com>
2023-07-11 09:00:10 -0400

    add solution

---
+++ 8kyu/forufcfans.js
@@ -0,0 +1,4 @@
+//For UFC Fans (Total Beginners): Conor McGregor vs George Saint Pierre
+var quote = function(fighter) {
+  return fighter.toLowerCase() === 'conor mcgregor' ? "I'd like to take this chance to apologize.. To absolutely NOBODY!" : 'I am not impressed by your performance.'
+};
