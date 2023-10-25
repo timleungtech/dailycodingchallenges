f1a06dbc97ae6c121770302c0f14c4b4c2176486
Tim Leung <timleungtech@gmail.com>
2023-10-25 10:28:14 -0400

    add solution

---
+++ 5kyu/ismyfriendcheating.js
@@ -0,0 +1,17 @@
+//Is my friend cheating?
+function removeNb (n) {
+  let arr = []
+  for (let i = 1; i <= n; i++) {
+    arr.push(i)
+  }
+  let sum = arr.reduce((a, c) => a + c)
+
+  let result = []
+  for (let a = 1; a < n; a++) {
+    let b = (sum - a) / (a + 1)
+    if (b % 1 === 0 && b < n) {
+      result.push([a, b])
+    }
+  }
+  return result
+}
