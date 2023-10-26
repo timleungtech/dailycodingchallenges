c64cdf6b27523df28458bb31ae9048ba74347fb1
timleungtech <hellopwntchu@gmail.com>
2023-10-26 12:50:39 -0400

    add solution

--- 5kyu/ismyfriendcheating.js
+++ 5kyu/ismyfriendcheating.js
@@ -1,5 +1,19 @@
 //Is my friend cheating?
 function removeNb (n) {
+  //Gauss sums
+  let sum = n*(n+1)/2
+
+  let result = []
+  for (let a = 1; a < n; a++) {
+    let b = (sum - a) / (a + 1)
+    if (b % 1 === 0 && b < n) {
+      result.push([a, b])
+    }
+  }
+  return result
+}
+
+function removeNb (n) {
   let arr = []
   for (let i = 1; i <= n; i++) {
     arr.push(i)
