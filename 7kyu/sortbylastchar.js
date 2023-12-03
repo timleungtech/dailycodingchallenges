dd10c83ca1582dd1ae60b2f547ca7f954f8f959b
Tim Leung <timleungtech@gmail.com>
2023-12-03 00:12:44 -0500

    add solution

--- 7kyu/sortbylastchar.js
+++ 7kyu/sortbylastchar.js
@@ -1,5 +1,9 @@
 //Sort by Last Char
 function last(x){
+  return x.split(' ').sort((a, b) => a.charCodeAt(a.length - 1) - b.charCodeAt(b.length - 1))
+}
+
+function last(x){
   x = x.split(' ').sort((a, b) => {
     const wordA = a.slice(-1)
     const wordB = b.slice(-1)
